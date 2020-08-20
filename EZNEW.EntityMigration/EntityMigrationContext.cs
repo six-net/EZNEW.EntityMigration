5ec45c8eea7e9b903c345017de7089ed424d2e58
DingBin.Li <lidingbin@live.com>
2020-08-20 19:23:02 +0800

    'Init'

---
+++ EZNEW.EntityMigration/EntityMigrationContext.cs
@@ -0,0 +1,60 @@
+﻿using EZNEW.Application;
+using System;
+using System.IO;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using EZNEW.Configuration;
+using EZNEW.Develop.Entity;
+using System.ComponentModel;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using EZNEW.ValueType;
+using EZNEW.Data;
+
+namespace EZNEW.EntityMigration
+{
+    public class EntityMigrationContext : DbContext
+    {
+        /// <summary>
+        /// Database type
+        /// </summary>
+        protected DatabaseServerType DatabaseType { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var entityConfigurations = EntityManager.GetAllEntityConfigurations();
+            foreach (var entityCfg in entityConfigurations)
+            {
+                var entityBuilder = modelBuilder.Entity(entityCfg.EntityType)
+                    .ToTable(entityCfg.TableName)
+                    .HasComment(entityCfg.Comment);
+                foreach (var field in entityCfg.AllFields.Values)
+                {
+                    var propertyBuilder = entityBuilder.Property(field.DataType, field.FieldName)
+                        .HasComment(field.Comment);
+
+                    //column type
+                    var columnTypeName = string.IsNullOrWhiteSpace(field.DbTypeName) ? EntityMigrationManager.GetColumnTypeName(DatabaseType, field.DataType) : field.DbTypeName;
+                    if (!string.IsNullOrWhiteSpace(columnTypeName))
+                    {
+                        propertyBuilder = propertyBuilder.HasColumnType(columnTypeName);
+                    }
+                    //maxlength
+                    if (field.MaxLength > 0)
+                    {
+                        propertyBuilder = propertyBuilder.HasMaxLength(field.MaxLength);
+                    }
+                    //allow null
+                    propertyBuilder = propertyBuilder.IsRequired(field.IsRequired || !field.DataType.AllowNull());
+                    //fixed length
+                    propertyBuilder = propertyBuilder.IsFixedLength(field.IsFixedLength);
+                    if (field.IsPrimaryKey)
+                    {
+                        entityBuilder.HasKey(field.FieldName);
+                    }
+                };
+            }
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ EZNEW.EntityMigration/EntityMigrationManager.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.EntityFrameworkCore.Query;
+using EZNEW.Data;
+
+namespace EZNEW.EntityMigration
+{
+    /// <summary>
+    /// Entity migration manager
+    /// </summary>
+    public static class EntityMigrationManager
+    {
+        internal static readonly Dictionary<DatabaseServerType, Dictionary<Guid, string>> ColumnTypeMaps = new Dictionary<DatabaseServerType, Dictionary<Guid, string>>();
+
+        public const string MigrationCommandObjectName = "EZNEWMigration";
+
+        static EntityMigrationManager()
+        {
+            ColumnTypeMaps = new Dictionary<DatabaseServerType, Dictionary<Guid, string>>()
+            {
+                {
+                    DatabaseServerType.SQLServer,
+                    new Dictionary<Guid, string>()
+                    {
+                        { typeof(string).GUID,"varchar(50)"}
+                    }
+                }
+            };
+        }
+
+        #region Column type
+
+        /// <summary>
+        /// Configure column type
+        /// </summary>
+        /// <param name="databaseServerType">Database type</param>
+        /// <param name="clrType">Clr type</param>
+        /// <param name="dbTypeName">Database type name</param>
+        public static void ConfigureColumnType(DatabaseServerType databaseServerType, Type clrType, string dbTypeName)
+        {
+            if (clrType == null)
+            {
+                return;
+            }
+            ConfigureColumnType(databaseServerType, new Dictionary<Guid, string>()
+            {
+                { clrType.GUID,dbTypeName}
+            });
+        }
+
+        /// <summary>
+        /// Configure column type
+        /// </summary>
+        /// <param name="databaseServerType">Database type</param>
+        /// <param name="columnTypeMaps">Column type maps</param>
+        public static void ConfigureColumnType(DatabaseServerType databaseServerType, IEnumerable<KeyValuePair<Guid, string>> columnTypeMaps)
+        {
+            if (columnTypeMaps.IsNullOrEmpty())
+            {
+                return;
+            }
+            if (ColumnTypeMaps.TryGetValue(databaseServerType, out var typeMaps))
+            {
+                foreach (var mapItem in columnTypeMaps)
+                {
+                    typeMaps[mapItem.Key] = mapItem.Value;
+                }
+            }
+            else
+            {
+                ColumnTypeMaps[databaseServerType] = columnTypeMaps.ToDictionary(c => c.Key, c => c.Value);
+            }
+        }
+
+        /// <summary>
+        /// Get column type name
+        /// </summary>
+        /// <param name="databaseServerType">Database server type</param>
+        /// <param name="clrType">Clr type</param>
+        public static string GetColumnTypeName(DatabaseServerType databaseServerType, Type clrType)
+        {
+            if (clrType == null)
+            {
+                return string.Empty;
+            }
+            if (ColumnTypeMaps.TryGetValue(databaseServerType, out var typeMaps) && typeMaps != null && typeMaps.TryGetValue(clrType.GUID, out var typeName))
+            {
+                return typeName;
+            }
+            return string.Empty;
+        }
+
+        #endregion
+    }
+}
